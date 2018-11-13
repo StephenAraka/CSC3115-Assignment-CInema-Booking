85e44103d8fcead8ec7fa021123afc94e212b6fd
StevenAraka <stevenaraka@gmail.com>
2018-11-13 20:58:24 +0300

    Add table and fileReader functionality

--- src/App.js
+++ src/App.js
@@ -1,103 +1,74 @@
-/* eslint-disable */
-import React, { Component, createElement } from 'react';
-import logo from './logo.svg';
-import './App.css';
-// import TableExampleComplex from "./Table";
+ /* eslint-disable */
+ import React, { Component, createElement } from 'react';
+ import BootstrapTable from 'react-bootstrap-table-next';
+ import logo from './logo.svg';
+ import './bootstrap/css/bootstrap.min.css';
+ import 'react-bootstrap-table-next/dist/react-bootstrap-table2.min.css';
+ import './App.css';
+ import { data, columns } from "./Utils";
 
-class App extends Component {
-    rows = ["1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20"];
-    constructor(props) {
-        super(props);
+ class App extends Component {
+     constructor(props) {
+         super(props);
+         this.readTextFile = this.readTextFile.bind(this);
+         this.handleChange = this.handleChange.bind(this);
+         this.handleSubmit = this.handleSubmit.bind(this);
 
-        // this.tasksArray = [object1];
-        this.state = {
-            seatStatus: "*"
-        }
-    }
-    render() {
-        return this.getHeader();
-        // this.getSeats();
-    }
+         this.state = {
+             prices: "",
+             welcome: "",
+             value: ""
+         }
+     }
 
-    getHeader = () => {
-        return (
-            createElement("div", { className: "App" },
-                createElement("header", { className: "App-header" },
-                    createElement("img", { src: logo, className: "App-logo", alt: logo }),
-                    createElement("h5", {}, "CSC 3112: Pinciples of Programming Languages - Assignment: Cinema Booking"),
-                    // TODO: add table looking
-                    createElement("div", { className: "vvip" },
-                        createElement("input", { type: "file", className: "openFile", onClick: this.fileReader }),
-                        createElement("p", { className: "price" }, "Very VIP = 100,000 shillings"),
-                        createElement("p", { className: "price" }, "VIP = 50,000 shillings"),
-                        createElement("p", { className: "price" }, "Twin Seats = 25,000 shillings"),
-                        createElement("p", { className: "price" }, "Economy = 20,000 shillings"),
-                        createElement("p", { className: "columnHeader" }, this.rows.toString()),
-                        ("A ", this.getButtons()),
-                        this.getButtons(),
-                        this.getButtons(),
-                        this.getButtons(),
-                        this.getButtons(),
-                        this.getButtons(),
-                        this.getButtons(),
-                        this.getButtons(),
-                        this.getButtons(),
-                        this.getButtons(),
-                        this.getButtons(),
-                        this.getButtons(),
-                        this.getButtons(),
-                        this.getButtons(),
-                        this.getButtons(),
-                        this.getButtons()
-                    )
-                )
-            )
-        );
-    }
+     render() {
+         return (
+             createElement("div", { className: "App" },
+                 createElement("header", { className: "App-header" },
+                     createElement("img", { src: logo, className: "App-logo", alt: logo }),
+                     createElement("h4", {}, "CSC 3112: Pinciples of Programming Languages - Assignment: Cinema Booking"),
+                     // TODO: add table looking
+                     createElement("div", { className: "Prices" },
+                         createElement("div", {}, "Please click to obtain the seats prices:",
+                             createElement("input", { type: "file", accept: "text/plain", onChange: this.readTextFile })),
+                     ),
+                     createElement("p", { className: "printPrices" }, this.state.prices),
+                     createElement(BootstrapTable, { className: "seatsTable", keyField: "id", data: data, columns: columns }),
+                     this.createForm()
+                 ))
+         );
+     }
+     readTextFile(event) {
+         var input = event.target;;
+         var reader = new FileReader();
+         reader.onload = () => {
+             var text = reader.result;
+             this.setState({ prices: text.substring(0, 200) });
+         };
+         if (input.files[0]) {
+             reader.readAsText(input.files[0]);
+         } else {
+             alert("Please choose a valid file!");
+             return;
+         }
+     }
 
-    fileReader = () => {
-        var fr = new FileReader();
-        fr.onload = () => {
-            createElement("pre", { className: "FileContents", textContent: this.result })
-        }
+     handleChange(event) {
+         this.setState({ value: event.target.value });
+     }
 
-    }
+     handleSubmit(event) {
+         alert('A name was submitted: ' + this.state.value);
+         event.preventDefault();
+     }
 
-    statePlay = () => {
-        if (this.state.seatStatus === "*") {
-            if (window.confirm("Would you like to book this seat?")) {
-                this.setState({
-                    seatStatus: "#"
-                })
-            } else return;
-        }
-    }
-
-    getButtons = () => {
-        var MaButton = createElement("p", { className: "paragraph" },
-            createElement("button", { onClick: this.statePlay }, this.state.seatStatus),
-            createElement("button", { onClick: this.statePlay }, this.state.seatStatus),
-            createElement("button", { onClick: this.statePlay }, this.state.seatStatus),
-            createElement("button", { onClick: this.statePlay }, this.state.seatStatus),
-            createElement("button", { onClick: this.statePlay }, this.state.seatStatus),
-            createElement("button", { onClick: this.statePlay }, this.state.seatStatus),
-            createElement("button", { onClick: this.statePlay }, this.state.seatStatus),
-            createElement("button", { onClick: this.statePlay }, this.state.seatStatus),
-            createElement("button", { onClick: this.statePlay }, this.state.seatStatus),
-            createElement("button", { onClick: this.statePlay }, this.state.seatStatus),
-            createElement("button", { onClick: this.statePlay }, this.state.seatStatus),
-            createElement("button", { onClick: this.statePlay }, this.state.seatStatus),
-            createElement("button", { onClick: this.statePlay }, this.state.seatStatus),
-            createElement("button", { onClick: this.statePlay }, this.state.seatStatus),
-            createElement("button", { onClick: this.statePlay }, this.state.seatStatus),
-            createElement("button", { onClick: this.statePlay }, this.state.seatStatus),
-            createElement("button", { onClick: this.statePlay }, this.state.seatStatus),
-            createElement("button", { onClick: this.statePlay }, this.state.seatStatus),
-            createElement("button", { onClick: this.statePlay }, this.state.seatStatus),
-            createElement("button", { onClick: this.statePlay }, this.state.seatStatus)
-        )
-        return MaButton;
-    }
-}
-
-export default App;
+     createForm = () => {
+         return (
+             createElement("form", { onSubmit: this.handleSubmit },
+                 createElement("label", {}, "Name:",
+                     createElement("input", { type: "text", value: this.state.value, onChange: this.handleChange })),
+                 createElement("input", { type: "submit", value: "Submit" }))
+         );
+     }
+ }
+ export default App;
---
+++ src/Utils.js
@@ -0,0 +1,43 @@
+export var data = [
+    { id: "A", number1: "*", number2: "*", number3: "*", number4: "*", number5: "*", number6: "*", number7: "*", number8: "*", number9: "*", number10: "*", number11: "*", number12: "*", number13: "*", number14: "*", number15: "*", number16: "*", number17: "*", number18: "*", number19: "*", number20: "*" },
+    { id: "B", number1: "*", number2: "*", number3: "*", number4: "*", number5: "*", number6: "*", number7: "*", number8: "*", number9: "*", number10: "*", number11: "*", number12: "*", number13: "*", number14: "*", number15: "*", number16: "*", number17: "*", number18: "*", number19: "*", number20: "*" },
+    { id: "C", number1: "*", number2: "*", number3: "*", number4: "*", number5: "*", number6: "*", number7: "*", number8: "*", number9: "*", number10: "*", number11: "*", number12: "*", number13: "*", number14: "*", number15: "*", number16: "*", number17: "*", number18: "*", number19: "*", number20: "*" },
+    { id: "D", number1: "*", number2: "*", number3: "*", number4: "*", number5: "*", number6: "*", number7: "*", number8: "*", number9: "*", number10: "*", number11: "*", number12: "*", number13: "*", number14: "*", number15: "*", number16: "*", number17: "*", number18: "*", number19: "*", number20: "*" },
+    { id: "E", number1: "*", number2: "*", number3: "*", number4: "*", number5: "*", number6: "*", number7: "*", number8: "*", number9: "*", number10: "*", number11: "*", number12: "*", number13: "*", number14: "*", number15: "*", number16: "*", number17: "*", number18: "*", number19: "*", number20: "*" },
+    { id: "F", number1: "*", number2: "*", number3: "*", number4: "*", number5: "*", number6: "*", number7: "*", number8: "*", number9: "*", number10: "*", number11: "*", number12: "*", number13: "*", number14: "*", number15: "*", number16: "*", number17: "*", number18: "*", number19: "*", number20: "*" },
+    { id: "G", number1: "*", number2: "*", number3: "*", number4: "*", number5: "*", number6: "*", number7: "*", number8: "*", number9: "*", number10: "*", number11: "*", number12: "*", number13: "*", number14: "*", number15: "*", number16: "*", number17: "*", number18: "*", number19: "*", number20: "*" },
+    { id: "H", number1: "*", number2: "*", number3: "*", number4: "*", number5: "*", number6: "*", number7: "*", number8: "*", number9: "*", number10: "*", number11: "*", number12: "*", number13: "*", number14: "*", number15: "*", number16: "*", number17: "*", number18: "*", number19: "*", number20: "*" },
+    { id: "I", number1: "*", number2: "*", number3: "*", number4: "*", number5: "*", number6: "*", number7: "*", number8: "*", number9: "*", number10: "*", number11: "*", number12: "*", number13: "*", number14: "*", number15: "*", number16: "*", number17: "*", number18: "*", number19: "*", number20: "*" },
+    { id: "J", number1: "*", number2: "*", number3: "*", number4: "*", number5: "*", number6: "*", number7: "*", number8: "*", number9: "*", number10: "*", number11: "*", number12: "*", number13: "*", number14: "*", number15: "*", number16: "*", number17: "*", number18: "*", number19: "*", number20: "*" },
+    { id: "K", number1: "*", number2: "*", number3: "*", number4: "*", number5: "*", number6: "*", number7: "*", number8: "*", number9: "*", number10: "*", number11: "*", number12: "*", number13: "*", number14: "*", number15: "*", number16: "*", number17: "*", number18: "*", number19: "*", number20: "*" },
+    { id: "L", number1: "*", number2: "*", number3: "*", number4: "*", number5: "*", number6: "*", number7: "*", number8: "*", number9: "*", number10: "*", number11: "*", number12: "*", number13: "*", number14: "*", number15: "*", number16: "*", number17: "*", number18: "*", number19: "*", number20: "*" },
+    { id: "M", number1: "*", number2: "*", number3: "*", number4: "*", number5: "*", number6: "*", number7: "*", number8: "*", number9: "*", number10: "*", number11: "*", number12: "*", number13: "*", number14: "*", number15: "*", number16: "*", number17: "*", number18: "*", number19: "*", number20: "*" },
+    { id: "N", number1: "*", number2: "*", number3: "*", number4: "*", number5: "*", number6: "*", number7: "*", number8: "*", number9: "*", number10: "*", number11: "*", number12: "*", number13: "*", number14: "*", number15: "*", number16: "*", number17: "*", number18: "*", number19: "*", number20: "*" },
+    { id: "O", number1: "*", number2: "*", number3: "*", number4: "*", number5: "*", number6: "*", number7: "*", number8: "*", number9: "*", number10: "*", number11: "*", number12: "*", number13: "*", number14: "*", number15: "*", number16: "*", number17: "*", number18: "*", number19: "*", number20: "*" },
+    { id: "P", number1: "*", number2: "*", number3: "*", number4: "*", number5: "*", number6: "*", number7: "*", number8: "*", number9: "*", number10: "*", number11: "*", number12: "*", number13: "*", number14: "*", number15: "*", number16: "*", number17: "*", number18: "*", number19: "*", number20: "*" }
+
+]
+
+export var columns = [
+    { dataField: 'id', },
+    { dataField: 'number1', text: 1 },
+    { dataField: 'number2', text: 2 },
+    { dataField: 'number3', text: 3 },
+    { dataField: 'number4', text: 4 },
+    { dataField: 'number5', text: 5 },
+    { dataField: 'number6', text: 6 },
+    { dataField: 'number7', text: 7 },
+    { dataField: 'number8', text: 8 },
+    { dataField: 'number9', text: 9 },
+    { dataField: 'number10', text: 10 },
+    { dataField: 'number11', text: 11 },
+    { dataField: 'number12', text: 12 },
+    { dataField: 'number13', text: 13 },
+    { dataField: 'number14', text: 14 },
+    { dataField: 'number15', text: 15 },
+    { dataField: 'number16', text: 16 },
+    { dataField: 'number17', text: 17 },
+    { dataField: 'number18', text: 18 },
+    { dataField: 'number19', text: 19 },
+    { dataField: 'number20', text: 20 }
+]
